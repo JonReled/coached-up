319e9fbb5f3a0cc1a2122f6cdbecf67e92502d35
JonReled <liam.joao.lopes@gmail.com>
2022-04-16 22:43:24 +0100

    Create app wrapper component, implement base home page structure

---
+++ src/app/pages/home-page/components/diet-overview/diet-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diet-overview',
+  templateUrl: './diet-overview.component.html',
+  styleUrls: ['./diet-overview.component.scss']
+})
+export class DietOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
